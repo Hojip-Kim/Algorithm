84e3c2370d8cf20fc33c68026864f56bb1cf5641
Hojip-Kim <101489057+Hojip-Kim@users.noreply.github.com>
2022-09-19 00:52:34 +0900

    [level 1] Title: 평균 구하기, Time: 0.04 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+        
+        for(int i=0; i<arr.length; i++){
+            answer += arr[i];
+        }
+        answer /= arr.length;
+
+        return answer;
+    }
+}
