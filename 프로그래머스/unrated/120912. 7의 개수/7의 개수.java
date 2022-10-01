10654a5c1b126ffce16c2fd548e27d0581aab131
Hojip-Kim <101489057+Hojip-Kim@users.noreply.github.com>
2022-10-01 16:56:09 +0900

    [unrated] Title: 7의 개수, Time: 0.03 ms, Memory: 78 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120912. 7의 개수/7의 개수.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        for(int i = 0; i<array.length; i++){
+            while(array[i] != 0){
+                if(array[i] % 10 == 7){
+                    answer++;
+                }
+                array[i] /= 10;
+                if(array[i] <1){
+                    break;
+                }
+            }
+
+        }
+        return answer;
+    }
+}
