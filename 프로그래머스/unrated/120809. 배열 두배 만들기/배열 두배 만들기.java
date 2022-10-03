7ce4a713cc4edc1142fb58be5a5e532e75cff032
Hojip-Kim <101489057+Hojip-Kim@users.noreply.github.com>
2022-10-03 23:14:01 +0900

    [unrated] Title: 배열 두배 만들기, Time: 0.04 ms, Memory: 71.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120809. 배열 두배 만들기/배열 두배 만들기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(int[] numbers) {
+        int[] answer = {};
+        
+        answer = numbers;
+        for (int i = 0; i < numbers.length; i++) {
+            
+            answer[i] *= 2;
+        }
+        return answer;
+    }
+}
