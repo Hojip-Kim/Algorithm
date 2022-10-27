6b0e8e1842f3e282d00d6c274a6475e3790eb986
Hojip-Kim <101489057+Hojip-Kim@users.noreply.github.com>
2022-10-27 23:57:32 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.04 ms, Memory: 70.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int n, int[] numlist) {
+        int[] answer = {};
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+
+        for (int i = 0; i < numlist.length; i++) {
+            if(numlist[i] % n == 0){
+                arr.add(numlist[i]);
+            }
+        }
+        
+        answer = new int[arr.size()];
+        for (int i = 0; i < arr.size(); i++) {
+            answer[i] = arr.get(i);
+        }
+
+        return answer;
+    }
+}
