1bd2540fa0fa0a3fcc43d61b54ae94126d54ac99
PEPEDOG <101489057+Hojip-Kim@users.noreply.github.com>
2023-08-23 11:48:34 +0900

    [Gold III] Title: 아침 산책, Time: 64 ms, Memory: 34200 KB -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.py
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -1,3 +1,4 @@
+
 import sys
 from collections import deque
 
@@ -9,36 +10,47 @@ isZero.append('0')
 for i in range(len(givenTF)) :
     isZero.append(givenTF[i])
 
-
+cnt = 0
 graph = [[] for _ in range(givenNum+1)]
 for _ in range(givenNum-1) :
     A, B = map(int,sys.stdin.readline().split())
     graph[A].append(B)
     graph[B].append(A)
+    if isZero[A] == '1' and isZero[B] == '1' :
+        cnt += 2
 
-cnt = 0
+visited_set = []
 
 def BFS(start) :
-    global cnt
+    tmpCnt = 0
+    indoor_visited = set()
+    visit = [False] * (givenNum + 1)
     q = deque([start])
-    if isZero[start] == '0' :
-        return
+    visit[start] = True
+    
+    while q :
+        tmp = q.popleft()
+        for i in graph[tmp] :
+            if visit[i] == False :
+                visit[i] = True
+                if isZero[i] == '1' : # 1 : 실내
+                    tmpCnt += 1
+                    indoor_visited.add(i)
+                elif isZero[i] == '0' : # 0 : 실외
+                    q.append(i)
+                    
+    if indoor_visited in visited_set :
+        return 0
+    visited_set.append(indoor_visited)
+    
+    if tmpCnt > 1 :
+        return tmpCnt * (tmpCnt-1)    
     else :
-        visit[start] = True
-        while q :
-            tmp = q.popleft()
-            for i in graph[tmp] :
-                if visit[i] == False :
-                    if isZero[i] == '1' :
-                        cnt += 1
-                    elif isZero[i] == '0' :
-                        visit[i] = True
-                        q.append(i)
-            
-
+        return 0
 
 
 for i in range(1, givenNum+1) :
-    visit = [False] * (givenNum + 1)
-    BFS(i)
+    if isZero[i] == '0' :
+        cnt += BFS(i)
+        
 print(cnt)
