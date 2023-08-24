21bd62f364734456ad8ebd0cbe8d81bcffdce61d
PEPEDOG <101489057+Hojip-Kim@users.noreply.github.com>
2023-08-24 17:02:52 +0900

    [Silver V] Title: 돌 게임, Time: 44 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.py
@@ -0,0 +1,24 @@
+import sys
+
+givenNum = int(sys.stdin.readline())
+
+if givenNum <= 2 :
+    if givenNum == 1 :
+        print('SK')
+    else :
+        print('CY')
+else :
+    boolList = [False] * (givenNum+1)
+    boolList[1] = True
+    boolList[2] = False
+    boolList[3] = True
+    for i in range(4, givenNum+1) :
+        if boolList[i-3] == True and boolList[i-1] == True :
+            boolList[i]=False
+        else :
+            boolList[i]=True
+
+    if boolList[givenNum] :
+        print('SK')
+    else :
+        print('CY')
