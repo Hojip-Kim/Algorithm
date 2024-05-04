17f6f34b425658d940247547866b369a99915a26
김호집 <101489057+Hojip-Kim@users.noreply.github.com>
2024-05-04 23:01:15 +0900

    [Silver IV] Title: 수 찾기, Time: 184 ms, Memory: 52008 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,31 +1,18 @@
 import sys
-import math
 
-n = int(sys.stdin.readline())
-givenArray = sorted(list(map(int,sys.stdin.readline().split())))
-m = int(sys.stdin.readline())
-findNums = list(map(int,sys.stdin.readline().split()))
+N = int(sys.stdin.readline())
 
-Ans = int()
+_list1 = list(map(int,sys.stdin.readline().split()))
 
-def binarySearch(left, right, target) :
-    global givenArray, Ans
-    if right < left :
-        return
+M = int(sys.stdin.readline())
+_list2 = list(map(int,sys.stdin.readline().split()))
 
-    mid = (left + right) // 2
-    if givenArray[mid] == target :
-        # print("찾음")
-        Ans = 1
-        return 
-    if givenArray[mid] < target :
-        binarySearch(mid+1, right, target)
+_dict = {}
+for i in _list1 :
+    _dict[i] = 1
+
+for j in _list2 :
+    if j in _dict :
+        print(1)
     else :
-        binarySearch(left, mid-1, target)
-    return
-
-
-for i in findNums :
-    Ans = 0
-    binarySearch(0, len(givenArray)-1, i)
-    print(Ans)
+        print(0)
