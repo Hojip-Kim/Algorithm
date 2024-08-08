4cfe7d599b7802b694975afa5cb4a4e0daddc3b2
김호집 <101489057+Hojip-Kim@users.noreply.github.com>
2024-08-08 20:42:32 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 1379.08 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -1,24 +1,6 @@
 class Solution {
-
-    private boolean[] arr = new boolean[1000001];
     public int solution(int[] elements) {
         int answer = 0;
-
-        for (int i = 1; i < elements.length + 1; i++) {
-            for (int j = 0; j < elements.length; j++) {
-                int num=0;
-                for (int k = j; k < j+i; k++) {
-                    num += elements[k % (elements.length)];
-                }
-                arr[num] = true;
-            }
-        }
-
-        for (int i = 0; i < arr.length; i++) {
-            if(arr[i]){
-                answer++;
-            }
-        }
         return answer;
     }
 }
