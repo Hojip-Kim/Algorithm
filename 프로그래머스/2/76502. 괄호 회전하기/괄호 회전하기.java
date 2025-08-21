9675699ab7ef345bd93d17dce7c14314c3ac4ea9
김호집 <101489057+Hojip-Kim@users.noreply.github.com>
2025-08-22 01:31:16 +0900

    [level 2] Title: 괄호 회전하기, Time: 37.01 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,35 +1,17 @@
 import java.util.*;
 
 class Solution {
+    
+    private Stack<String> stack;
+    
     public int solution(String s) {
-        int n = s.length();
-        int answer = 0;
+        int answer = -1;
         
-        for (int i = 0; i < n; i++) {
-            String rotated = s.substring(i) + s.substring(0, i); // i만큼 회전
-            if (isValid(rotated)) answer++;
-        }
+        stack = new Stack<>();
+        
+        
         
         return answer;
-    }
-    
-    private boolean isValid(String str) {
-        Stack<Character> stack = new Stack<>();
         
-        for (char c : str.toCharArray()) {
-            if (c == '(' || c == '{' || c == '[') {
-                stack.push(c);
-            } else {
-                if (stack.isEmpty()) return false;
-                char top = stack.pop();
-                if ((c == ')' && top != '(') ||
-                    (c == '}' && top != '{') ||
-                    (c == ']' && top != '[')) {
-                    return false;
-                }
-            }
-        }
-        
-        return stack.isEmpty();
     }
 }
